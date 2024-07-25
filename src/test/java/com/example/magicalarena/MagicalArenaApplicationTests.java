e0d90ecd4a48997ed7fb707831579729cdd697fb
Arun <parun1331@gmail.com>
2024-07-25 22:46:16 +0530

    tested for player and AttackDamage

--- src/test/java/com/example/magicalarena/MagicalArenaApplicationTests.java
+++ src/test/java/com/example/magicalarena/MagicalArenaApplicationTests.java
@@ -13,6 +13,21 @@ class MagicalArenaApplicationTests {
 
 
 
+    @Test
+    public void testPlayerCreation() {
+        Player player = new Player("A", 50, 5, 10, PlayerType.HUMAN);
+        assertEquals("A", player.getName());
+        assertEquals(50, player.getHealth());
+        assertEquals(5, player.getStrength());
+        assertEquals(10, player.getAttack());
+        assertEquals(PlayerType.HUMAN, player.getPlayerType());
 
+    }
+    @Test
+    public void testCalculateAttackDamage() {
+        Player player = new Player("A", 50, 5, 10,PlayerType.HUMAN);
+        int damage = player.calculateAttackDamage();
+        assertTrue(damage >= 10 && damage <= 60); // 1*10 to 6*10
+    }
 
 }
